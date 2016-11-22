f03c58a53187c5dbffe06ffc3c77857269b3f184
David Goldsmith <David.Goldsmith@ForgeRock.com>
2016-11-22 15:45:50 -0800

    Fix problems with config.sh and amster.sh

--- openam-config/config.sh
+++ openam-config/config.sh
@@ -8,7 +8,7 @@
 export SERVER_URL=${OPENAM_INSTANCE:-http://openam:80}
 export URI=${SERVER_URI:-/openam}
 
-export INSTANCE="${SERVER_URL}/${URI}"
+export INSTANCE="${SERVER_URL}${URI}"
 
 # Alive check
 ALIVE="${INSTANCE}/isAlive.jsp"
@@ -69,7 +69,6 @@ fi
 echo "Running Configurator"
 java -jar /var/tmp/ssoconfig/openam-configurator-tool*.jar -f /var/tmp/config/openam.properties
 
-
 if [ -x /var/tmp/amster.sh ]; then
 echo "Executing REST commands"
    /var/tmp/amster.sh
@@ -78,6 +77,7 @@ fi
 #curl 'http://openam.test.com/openam/json/global-config/realms/root?_action=create' -H 'Accept-API-Version: protocol=1.0,resource=1.0' -H 'Cookie: JSESSIONID=35824C25B893D4D8503F3501E9C0DBBC; amlbcookie=01; iPlanetDirectoryPro=AQIC5wM2LY4SfcwHJ_XHBHeDxe61rnaRgXhOuiqsxqxcCwk.*AAJTSQACMDIAAlNLABQtNzA4OTM3NDA1NjE1MjIwNjgwNAACUzEAAjAx*; i18next=en-US' -H 'Origin: http://openam.test.com' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: en-US,en;q=0.8' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.52 Safari/537.36' -H 'Content-Type: application/json' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Cache-Control: no-cache' -H 'Referer: http://openam.test.com/openam/XUI/?realm=/' -H 'DNT: 1' --data-binary '{"name":"test","parentPath":"/","aliases":["foo.com"],"active":true}' --compressed
 
 
+echo ""
 echo "Done. This container will sleep for a while, but you can now safely undeploy it"
 # For debugging purposes it is handy to leave the container running by sleeping
 sleep 5000
